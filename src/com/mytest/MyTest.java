e3103ad515dfa9768db4423548708f6c2af60277
Chapaev Anton <virsus2009@gmail.com>
2020-10-13 21:11:08 +0700

    Поправил ООП
    *если я правильно понял.

--- src/com/mytest/MyTest.java
+++ src/com/mytest/MyTest.java
@@ -1,20 +1,31 @@
 package com.mytest;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MyTest {
     public static void main(String[] args) {
         //задание 1
-        User tester = new User(7, "Developer");
-        System.out.println(tester.getId());
-        System.out.println(tester.getVacancy());
+
+        Vacancy[] vacancies = {new Vacancy("Developer"),new Vacancy("Engineer")};
+        ArrayList<Vacancy> vacanciesBase = new ArrayList<>(Arrays.asList(vacancies));
+        vacanciesBase.add(new Vacancy("Programmer"));
+        vacanciesBase.add(new Vacancy("Сleaner"));
+
+        ArrayList<User> usersBase = new ArrayList<User>();
+
+        usersBase.add(new User(1, vacanciesBase.get(0)));
+        usersBase.add(new User(2, vacanciesBase.get(3)));
+
+
+        showConsole(usersBase);
 
 
         //задание 2 прайс лист и скидки
         int[] price = {5, 100, 20, 66, 16};
 
-        MyTask2 tester1 = new MyTask2();
-        price = tester1.decryptData(price, 50, 1, 3);
+        MyTask2 upData = new MyTask2();
+        price = upData.decryptData(price, 50, 1, 3);
 
         String newPrice = Arrays.toString(price);
 
@@ -22,28 +33,41 @@ public class MyTest {
 
     }
 
+    private static void showConsole(ArrayList<User> usersBase) {
+        for (int i = 0; i < usersBase.size(); i++) {
+            System.out.println(usersBase.get(i).getId());
+            System.out.println(usersBase.get(i).getVacancy());
+        }
+    }
+
     static class User {
         private long id;
-        private String str;
-        //private Vacancy vacancy;
+        private Vacancy vacancy;
 
-        User(long id, String str) {
-            this.str = str;
+        User(long id, Vacancy vacancy) {
+            this.vacancy = vacancy;
             this.id = id;
         }
 
         String getVacancy() {
-            return Vacancy.getVacancy(str);
+            return vacancy.getVacancy();
         }
 
         long getId() {
             return id;
         }
+
     }
 
     static class Vacancy {
-        static String getVacancy(String str) {
-            return str;
+        String nameVacancy;
+
+        Vacancy(String nameVacancy) {
+            this.nameVacancy = nameVacancy;
+        }
+
+        String getVacancy() {
+            return nameVacancy;
         }
     }
 }
