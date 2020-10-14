2ea96bde0f581fbd036855608e769161278ae466
ChapaevAnton <virsus2009@gmail.com>
2020-10-14 11:01:08 +0700

    Это уже лишнее...2

--- src/com/mytest/MyTest.java
+++ src/com/mytest/MyTest.java
@@ -6,19 +6,30 @@ import java.util.Arrays;
 public class MyTest {
     public static void main(String[] args) {
         //задание 1
+        User tester = new User(7, new Vacancy("Developer"));
+        System.out.println(tester.getId());
+        System.out.println(tester.getVacancy());
 
+        //первоначальный массив вакансии
         Vacancy[] vacancies = {item("Developer"), item("Engineer")};
+        //список вакнасии на основе первоначального массива
         ArrayList<Vacancy> vacanciesBase = new ArrayList<>(Arrays.asList(vacancies));
+        //дополнительные вакансии
         vacanciesBase.add(item("Programmer"));
         vacanciesBase.add(item("Сleaner"));
 
+        //список кандидатов
         ArrayList<User> usersBase = new ArrayList<User>();
 
-        usersBase.add(user(1, 0, vacanciesBase));
-        usersBase.add(user(2, 10, vacanciesBase));
-        usersBase.add(user(3, 1, vacanciesBase));
-        usersBase.add(user(4, 2, vacanciesBase));
-
+        //добавляем кандидата на соответсвующую вакансию
+        usersBase.add(user(0, 0, vacanciesBase));
+        usersBase.add(user(1, 10, vacanciesBase));
+        usersBase.add(user(2, 1, vacanciesBase));
+        usersBase.add(user(3, 2, vacanciesBase));
+        //если вдруг нет вакансии в базе но надо временную создать не из  общего пула
+        usersBase.add(user(4, item("temp_vacancy")));
+        usersBase.add(new User(5, new Vacancy("temp_vacancy1")));
+        //выводим базу кандидатов
         showConsole(usersBase);
 
     }
@@ -27,12 +38,16 @@ public class MyTest {
         return new Vacancy(nameVacancy);
     }
 
-    private static User user(int id, int idVacancy, ArrayList<Vacancy> baseVacancy) {
+    private static User user(int idUser, int idVacancy, ArrayList<Vacancy> baseVacancy) {
         try {
-            return new User(id, baseVacancy.get(idVacancy));
+            return new User(idUser, baseVacancy.get(idVacancy));
         } catch (IndexOutOfBoundsException e) {
             return null;
         }
+    }
+
+    private static User user(int idUser, Vacancy nameVacancy) {
+        return new User(idUser, nameVacancy);
     }
 
     private static void showConsole(ArrayList<User> usersBase) {
@@ -41,7 +56,8 @@ public class MyTest {
             if (usersBase.get(i) != null) {
                 System.out.println(usersBase.get(i).getId());
                 System.out.println(usersBase.get(i).getVacancy());
-            } else System.out.println("Такой вакансии не существует");
+            } else
+                System.out.println("Вакансии с индексом[" + i + "]\nне существует в базе, пользователь не создан...");
         }
 
     }
